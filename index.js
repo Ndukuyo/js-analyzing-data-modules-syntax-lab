73d9e779671c4272b153191d1505199035a60082
Ndukuyo <harrisonndukuyo@gmail.com>
2025-12-19 17:37:01 +0300

    innitial commit

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+// require date module
+const DateJS = require('datejs');
 
+function combineUsers(...args){
 
+  const combinedObject = {
+    users: []
+  };
+
+  // step 2: loop through args and merge the arrays
+  for(const array of args) {
+
+    // step 3 & 4: make sure we're working with an array and spread it into the users array 
+    if(Array.isArray(array)) {
+      combinedObject.users.push(...array);
+    }
+  }
+
+  // step 5: add todays date 
+  const today = new Date()
+  const formatDate = today.toString('M/d/yyyy')
+  combinedObject.merge_date = formatDate;
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
